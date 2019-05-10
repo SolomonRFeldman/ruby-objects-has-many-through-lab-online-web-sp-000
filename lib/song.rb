3aac82ed7c3ebad43304c7611ecfdd40648d0124
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-10 19:44:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,14 @@
 class Song
   attr_reader :name, :artist, :genre
   
+  @@all = []
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     artist.songs << self
+    @@all << self
   end    
 
 end
