02fd39e596c8078abadef074b08a3bc9488f03b4
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-10 19:58:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,9 +11,7 @@ class Artist
   end
   
   def new_song(name, genre)
-    new_song = Song.new(name, self, genre)
-    @songs << new_song
-    new_song
+    Song.new(name, self, genre)
   end
   
   def self.all
